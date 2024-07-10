837756653ed3ae080c205a9447e8882ecb610e08
YugShrivastava <shrivastavayug04@gmail.com>
2024-07-10 23:45:09 +0530

    Started adding functionality to addTask button, have to separate it into modules.

--- src/scripts/addTask.js
+++ src/scripts/addTask.js
@@ -1,6 +1,6 @@
 import { addTaskDiv } from "./allSidebarContent";
 
-//
+// have to make separate modules/files for creating new todos, setting todos as complete, changing todo priority and DOM stuff
 addTaskDiv.addEventListener("click", () => {
     console.log("Add Task Clicked.");
 });
