223cede64ca9cfa8a5ec8a174619041476efe71a
YugShrivastava <shrivastavayug04@gmail.com>
2024-10-11 01:13:30 +0530

    localStorage code added...

--- src/scripts/getTaskDetails.js
+++ src/scripts/getTaskDetails.js
@@ -5,11 +5,13 @@ function getTaskObj() {
     const project = document.getElementById("task-project");
     const priority = document.getElementsByName("priority");
     const notes = document.getElementById("task-notes");
+
     let index;
-    for(let i = 0; i < priority.length; i++){
-        if(priority[i].checked){
+    for (let i = 0; i < priority.length; i++) {
+        if (priority[i].checked) {
             index = i;
-            break;}
+            break;
+        }
     }
 
     const obj = {
@@ -19,46 +21,48 @@ function getTaskObj() {
         project: project.value,
         priority: priority[index].value,
         notes: notes.value,
-    }
+    };
     return obj;
 }
 
 function storageAvailable(type) {
     let storage;
     try {
-      storage = window[type];
-      const x = "__storage_test__";
-      storage.setItem(x, x);
-      storage.removeItem(x);
-      return true;
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
     } catch (e) {
-      return (
-        e instanceof DOMException &&
-        e.name === "QuotaExceededError" &&
-        // acknowledge QuotaExceededError only if there's something already stored
-        storage &&
-        storage.length !== 0
-      );
+        return (
+            e instanceof DOMException &&
+            e.name === "QuotaExceededError" &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage &&
+            storage.length !== 0
+        );
     }
 }
 
-function populateStorage(obj){
-    sessionStorage.setItem("tasks", obj); 
+function populateStorage(obj) {
+    localStorage.setItem("tasks", obj);
 }
 document.getElementById("task-form").addEventListener("submit", (e) => {
     e.preventDefault();
     const newTask = getTaskObj();
 
-    if(storageAvailable(sessionStorage)){
-        if(sessionStorage.getItem("tasks") == null) populateStorage([newTask]);
-        else{
-            const allTasks = sessionStorage.getItem("tasks");
+    if (storageAvailable(localStorage)) {
+        if (localStorage.getItem("tasks") == null) populateStorage([newTask]);
+        else {
+            const allTasks = localStorage.getItem("tasks");
             allTasks[allTasks.length] = newTask;
             populateStorage(allTasks);
         }
+    } else {
+        console.log("Space is not available");
+        localStorage.clear;
     }
 
-
-    const getTasks = JSON.parse(sessionStorage.getItem("tasks"))
+    const getTasks = JSON.parse(localStorage.getItem("tasks"));
     console.log(getTasks);
-})
+});
